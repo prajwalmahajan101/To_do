bf35d315894ffe7f3f1f7bd445e683caadc869f6
Prajwal Mahajan <prajwal.mahajan101@gmail.com>
2022-05-16 16:38:34 +0530

    Basic Action For Task Like Delete

--- controllers/tasks_controller.js
+++ controllers/tasks_controller.js
@@ -15,3 +15,34 @@ module.exports.create = async (req,res)=>{
     res.redirect('back');
 
 }
+
+
+module.exports.distroy = async (req,res)=>{
+    // console.log(req.params.id);
+    try{
+        let task = await Task.findById(req.params.id);
+        task.remove();
+        req.flash('success','Task Deleted Sucessfully !');
+    }
+    catch(err){
+        req.flash('error','Sorry, Task Cannot Be Deleted !!');
+        console.log(err);
+    }
+    res.redirect('back');
+
+} 
+
+module.exports.complete = async (req,res)=>{
+    // console.log(req.params.id);
+    try{
+        let task = await Task.findById(req.params.id);
+        task.isCompleted = true;
+        task.save();
+        req.flash('success','Task Marked Complete Sucessfully !');
+    }
+    catch(err){
+        req.flash('error','Sorry, Task Cannot Be Marked Complete !!');
+        console.log(err);
+    }
+    res.redirect('back');
+}
--- routes/tasks.js
+++ routes/tasks.js
@@ -6,6 +6,8 @@ const passport = require('passport');
 const taskController = require('../controllers/tasks_controller') 
 
 router.post('/create', passport.checkAuthentication,taskController.create);
+router.get('/delete/:id', passport.checkAuthentication,taskController.distroy);
+router.get('/complete/:id', passport.checkAuthentication,taskController.complete);
 
 
 module.exports = router;
